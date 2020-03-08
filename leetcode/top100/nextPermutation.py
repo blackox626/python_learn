920b1991eda5e28b6f7df97015ec26bfe6203617
chenzijie <chenzijie@weidian.com>
2020-03-08 21:21:36 +0800

    nextPermutation  简单优化一下

--- leetcode/top100/nextPermutation.py
+++ leetcode/top100/nextPermutation.py
@@ -25,5 +25,29 @@ class Solution(object):
 
         return nums.sort()
 
+    # 还可以 优化下 无需冒泡 只需reverse 一下 就好
 
-print(Solution().nextPermutation([5, 4, 7, 5, 3, 2]))
+    def nextPermutation2(self, nums):
+
+        if nums is None or len(nums) == 0:
+            return None
+        for i in range(len(nums) - 1, 0, -1):
+            if nums[i] > nums[i - 1]:
+                # reverse
+
+                l = i
+                r = len(nums) - 1
+                while l < r:
+                    nums[l], nums[r] = nums[r], nums[l]
+                    l += 1
+                    r -= 1
+
+            for t in range(i, len(nums)):
+                if nums[t] > nums[i - 1]:
+                    nums[t], nums[i - 1] = nums[i - 1], nums[t]
+                    return nums
+
+        return nums.sort()
+
+
+print(Solution().nextPermutation2([5, 4, 7, 5, 3, 2]))
