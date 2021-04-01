e1dcc00a6b6060c4601bf35fc945401bf7d5ebfc
chenzijie <chenzijie@weidian.com>
2021-04-01 20:21:59 +0800

    滑动窗口

--- leetcode/top100/longsubstr.py
+++ leetcode/top100/longsubstr.py
@@ -28,5 +28,22 @@ class Solution(object):
 
         return maxCount
 
+    def lengthOfLongestSubstring2(self, s: str):
+        if not s: return 0
+        left = 0
+        lookup = set()
+        n = len(s)
+        max_len = 0
+        cur_len = 0
+        for i in range(n):
+            cur_len += 1
+            while s[i] in lookup:
+                lookup.remove(s[left])
+                left += 1
+                cur_len -= 1
+            if cur_len > max_len: max_len = cur_len
+            lookup.add(s[i])
+        return max_len
+
 
 Solution().lengthOfLongestSubstring("anaconda3")
