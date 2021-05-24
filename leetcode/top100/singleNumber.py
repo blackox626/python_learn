b6704952b949b53ea7c780c27abf86f4e3f4611d
chenzijie <chenzijie@weidian.com>
2021-05-24 17:25:02 +0800

    singleNumber

--- leetcode/top100/singleNumber.py
+++ leetcode/top100/singleNumber.py
@@ -1,3 +1,4 @@
+import collections
 from functools import reduce
 from typing import List
 
@@ -23,3 +24,29 @@ class Solution(object):
             else:
                 b ^= n
         return [a, b]
+
+    """
+    给你一个整数数组 nums ，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次 。请你找出并返回那个只出现了一次的元素。
+    """
+
+    def singleNumber_3(self, nums: List[int]) -> int:
+        ans = 0
+        for i in range(32):
+            total = sum((num >> i) & 1 for num in nums)
+            if total % 3:
+                # Python 这里对于最高位需要特殊判断
+                if i == 31:
+                    ans -= (1 << i)
+                else:
+                    ans |= (1 << i)
+        return ans
+
+    def singleNumber_3_1(self, nums: List[int]) -> int:
+        freq = collections.Counter(nums)
+        ans = [num for num, occ in freq.items() if occ == 1][0]
+        return ans
+
+
+freq = collections.Counter('abcdabc')
+for alpha, occ in freq.items():
+    print(alpha, occ)
